4313f1f68cba022ba61cf5cf21ffb95a66511abf
YeisonCordoba07 <yalexander.cordoba@gmail.com>
2026-01-20 19:28:09 -0500

    add Empleado class

--- mision1/proyecto1/src/com/empresa/gestion/model/Empleado.java
+++ mision1/proyecto1/src/com/empresa/gestion/model/Empleado.java
@@ -1,5 +1,31 @@
 package com.empresa.gestion.model;
 
 public class Empleado extends Persona{
+    private double salario;
+
+
+
+    public Empleado(String nombre, int edad, double salario){
+        super(nombre, edad);
+        this.salario = salario;
+    }
+
+
+
+    public double getSalario(){
+        return salario;
+    }
+
+    public void setSalario(double salario){
+        this.salario = salario;
+    }
+
+
+
+    @Override
+    public void presentarse() {
+        System.out.println("Soy el empleado " + getNombre() + " y misalario es " + salario);
+    }
+
     
 }
