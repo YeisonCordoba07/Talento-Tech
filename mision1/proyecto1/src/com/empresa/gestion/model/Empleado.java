9df26da65ca1df67fde256875751af721d450b88
YeisonCordoba07 <yalexander.cordoba@gmail.com>
2026-01-24 14:48:47 -0500

    add bussines logic to Empleado class

--- mision1/proyecto1/src/com/empresa/gestion/model/Empleado.java
+++ mision1/proyecto1/src/com/empresa/gestion/model/Empleado.java
@@ -2,6 +2,7 @@ package com.empresa.gestion.model;
 
 public class Empleado extends Persona{
     private double salario;
+    private int horasTrabajadas;
 
 
 
@@ -20,11 +21,37 @@ public class Empleado extends Persona{
         this.salario = salario;
     }
 
+    public int getHorasTrabajadas(){
+        return horasTrabajadas;
+    }
+
+    public void setHorasTrabajadas(int horasTrabajadas){
+        this.horasTrabajadas = horasTrabajadas;
+    }
+
+
 
 
     @Override
     public void presentarse() {
         System.out.println("Soy el empleado " + getNombre() + " y mi salario es " + salario);
+    }
+
+
+
+    public double calcularSalarioMensual(){
+        double valorHora = salario / 160;
+        double salarioFinal = valorHora * horasTrabajadas;
+        return salarioFinal;
+    }
+
+    public boolean esMayorDeEdad(){
+
+        return getEdad() >= 18;
+    }
+
+    public boolean puedeTrabajar(){
+        return esMayorDeEdad() && isActivo();
     }
 
     
