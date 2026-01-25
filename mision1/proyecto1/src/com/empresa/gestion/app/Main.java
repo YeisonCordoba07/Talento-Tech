b479ae85edafc39793d0530e60ec6fb7feb5ed55
YeisonCordoba07 <yalexander.cordoba@gmail.com>
2026-01-24 21:03:46 -0500

    fix Empleado and Gerente creation object

--- mision1/proyecto1/src/com/empresa/gestion/app/Main.java
+++ mision1/proyecto1/src/com/empresa/gestion/app/Main.java
@@ -7,9 +7,9 @@ import com.empresa.gestion.service.NominaService;
 public class Main {
 
     public static void main(String[] args){
-        Empleado empleado1 = new Empleado("Yeison", 18, 200.0);
+        Empleado empleado1 = new Empleado("Yeison", 18, 'M', true, 3200.0, 160);
 
-        Gerente gerente1 = new Gerente("Juan", 40, 100, "SRE");
+        Gerente gerente1 = new Gerente("Juan", 40, 'M', true, 5000.0, 160, "SRE", 1000.0);
 
         empleado1.presentarse();
         gerente1.presentarse();
