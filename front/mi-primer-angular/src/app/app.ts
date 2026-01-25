563fca5c118514306d701bceb20e95f6dd5830a0
YeisonCordoba07 <yalexander.cordoba@gmail.com>
2026-01-24 22:32:03 -0500

    show users in user component

--- front/mi-primer-angular/src/app/app.ts
+++ front/mi-primer-angular/src/app/app.ts
@@ -1,32 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { UserService } from './services/user.service/user.service';
-
-import { JsonPipe } from '@angular/common';
-import { ChangeDetectorRef } from '@angular/core';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, JsonPipe],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('mi-primer-angular');
 
-  users: any[] = [];
 
-  constructor(private userService: UserService, private changeDetector: ChangeDetectorRef){
-
-    this.userService.getUsers().subscribe(data =>{
-        this.users = data;
-        console.log(this.users);
-        //this.detectChange();
-    })
-  }
-
-  detectChange(){
-    this.changeDetector.detectChanges();
-  }
 }
--- front/mi-primer-angular/src/app/view/user.component/user.component.ts
+++ front/mi-primer-angular/src/app/view/user.component/user.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
+import { UserService } from '../../services/user.service/user.service';
+import { CommonModule, JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-user.component',
-  imports: [],
+  imports: [JsonPipe, CommonModule],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
 })
 export class UserComponent {
 
+  users: any[] = [];
+
+  constructor(private userService: UserService, private changeDetector: ChangeDetectorRef){
+
+    this.userService.getUsers().subscribe(data =>{
+        this.users = data;
+        console.log(this.users);
+        this.detectChange();
+    })
+  }
+
+  detectChange(){
+    this.changeDetector.detectChanges();
+  }
 }
